8899fd8864c422ce698b48894ce55e3a3a796dfe
S2Ayaka <linh.huynh.dev.99@gmail.com>
2023-09-04 17:47:10 +0700

    random effect

--- controller.js
+++ controller.js
@@ -1,6 +1,7 @@
 // Initialize an empty array for jsonData
 let jsonData = [];
-
+let timeRandom = 1000;
+let randomEffect;
 // Function to fetch JSON data from tableJson.json
 function fetchJsonData() {
     fetch('tableJson.json')
@@ -20,6 +21,20 @@ function getRandomIndex(max) {
     return Math.floor(Math.random() * max);
 }
 
+// Function to animate an element with a fade-in effect
+function animateFadeIn(element) {
+    element.style.opacity = 0; // Start with 0 opacity
+
+    // Trigger the animation by adding a CSS class
+    element.classList.add("fade-in");
+
+    // Remove the CSS class and set opacity to 1 after a delay to reset the animation
+    setTimeout(() => {
+        element.style.opacity = 1; // Set opacity to 1
+        element.classList.remove("fade-in");
+    }, timeRandom);
+}
+
 // Function to update the random data with animation
 function updateRandomData() {
     if (jsonData.length === 0) {
@@ -27,37 +42,62 @@ function updateRandomData() {
         return;
     }
 
-    // Disable the button for 1 second
     generateButton.disabled = true;
     setTimeout(() => {
         generateButton.disabled = false;
-    }, 1000);
+    }, timeRandom);
 
-    // Get a random item from the JSON data
+    randomEffect = setInterval(changeRandomImage, 10);
+
+    setTimeout(() => {
+        clearInterval(randomEffect);
+    }, timeRandom);
+
     const randomIndex = getRandomIndex(jsonData.length);
     const randomItem = jsonData[randomIndex];
 
-    // Create a fade-in effect for the random image
     const randomImageElement = document.getElementById("randomImage");
-    randomImageElement.style.opacity = 0; // Start with 0 opacity
-    randomImageElement.src = randomItem.src;
-
-    // Create a fade-in effect for the random name
     const randomNameElement = document.getElementById("randomName");
-    randomNameElement.style.opacity = 0; // Start with 0 opacity
-    randomNameElement.textContent = randomItem.name;
+    const docIconElement = document.getElementById("icon_element");
+    const docNameElement = document.getElementById("name_element");
+    const docIconWeapon = document.getElementById("icon_weapon");
+    const docNameWeapon = document.getElementById("name_weapon");
+    const docIconStar = document.getElementById("icon_star");
 
-    // Trigger the animation by adding a CSS class
-    randomImageElement.classList.add("fade-in");
-    randomNameElement.classList.add("fade-in");
+    randomImageElement.src = randomItem.src; // Set the image source
+    randomNameElement.textContent = randomItem.name; // Set the text content
 
-    // Remove the CSS class after a delay to reset the animation
-    setTimeout(() => {
-        randomImageElement.style.opacity = 1; // Set opacity to 1
-        randomNameElement.style.opacity = 1; // Set opacity to 1
-        randomImageElement.classList.remove("fade-in");
-        randomNameElement.classList.remove("fade-in");
-    }, 1000);
+    docIconElement.src = randomItem.ngtoUrl;
+    docNameElement.textContent = randomItem.element;
+    docIconWeapon.src = randomItem.weaponUrl;
+    docNameWeapon.textContent = randomItem.weapon;
+    docIconStar.src = randomItem.starUrl;
+
+    animateFadeIn(randomImageElement);
+    animateFadeIn(randomNameElement);
+}
+
+function changeRandomImage() {
+    const randomIndex = getRandomIndex(jsonData.length);
+    const randomItem = jsonData[randomIndex];
+
+    const randomImageElement = document.getElementById("randomImage");
+    const randomNameElement = document.getElementById("randomName");
+    const docIconElement = document.getElementById("icon_element");
+    const docNameElement = document.getElementById("name_element");
+    const docIconWeapon = document.getElementById("icon_weapon");
+    const docNameWeapon = document.getElementById("name_weapon");
+    const docIconStar = document.getElementById("icon_star");
+
+    randomImageElement.src = randomItem.src; // Set the image source
+    randomNameElement.textContent = randomItem.name; // Set the text content
+
+    docIconElement.src = randomItem.ngtoUrl;
+    docNameElement.textContent = randomItem.element;
+    docIconWeapon.src = randomItem.weaponUrl;
+    docNameWeapon.textContent = randomItem.weapon;
+    docIconStar.src = randomItem.starUrl;
+
 }
 
 // Add event listener to the button
--- getChar.py
+++ getChar.py
@@ -14,22 +14,43 @@ data_list = []
 
 # Find the table body
 tbody = table.find("tbody")
-
+count_char = 0
 # Iterate through the rows of the table body
 for row in tbody.find_all("tr"):
     columns = row.find_all("td")
     src = columns[0].find("img")["src"]
     name = columns[1].get_text(strip=True)
     match = re.search(r'(https?://.*?\.png)', src)
+    star = columns[2].find("img")["title"]
+    starUrl = columns[2].find("img")["src"].split(".png")[0] + ".png"
+    check_ngto = columns[3].find("a", {"title": True})
+    ngto = None
+    ngtoUrl = None
+    if (str(check_ngto) != "None"):
+        ngto = columns[3].find("a")["title"]
+        ngtoUrl = columns[3].find("img")["src"].split(".svg")[0] + ".svg"
+    else:
+        print(check_ngto)
+    weapon = columns[4].find("a")["title"]
+    weaponUrl = columns[4].find("img")["src"].split(".png")[0] + ".png"
+    count_char += 1
+
     if match:
         extracted_string = match.group(1)
-        print(extracted_string)
-        save = {"src": extracted_string, "name": name}
-        print(save)
+        save = {
+            "src": extracted_string,
+            "name": name,
+            "star": star,
+            "element": ngto,
+            "weapon": weapon,
+            "starUrl": starUrl,
+            "ngtoUrl": ngtoUrl,
+            "weaponUrl": weaponUrl,
+        }
         data_list.append(save)
     else:
         print("'.png' not found in the URL")
-        
+
 
 # Write the extracted data to a JSON file
 with open("tableJson.json", "w", encoding="utf-8") as json_file:
